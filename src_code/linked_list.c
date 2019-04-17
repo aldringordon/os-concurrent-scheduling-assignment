66a8baa2c32b0ed6c24a69a026aa61d4b7a029cd
Aldrin Gordon <aldrin.gordon@student.curtin.edu.au>
2019-04-18 01:28:48 +0800

    Adding basic files. Created task_file generator in Python so I dont have to manually make the file. Created a README which describes how the simulation will run hopefully

---
+++ src_code/linked_list.c
@@ -0,0 +1,8 @@
+/**
+ * Aldrin Gordon
+ * Student No: 18340587
+ * File:
+ * Purpose:
+ */
+#include <std.io>
+#include <stdlib.h>
---
+++ src_code/scheduler.c
@@ -0,0 +1,12 @@
+/**
+ * Aldrin Gordon
+ * Student No: 18340587
+ * File: scheduler.c
+ * Purpose: Main Program that contains the tasks
+ * in the assignment specification.
+ *
+ * Specified required functions:
+ *  - task() : gets two tasks at a time and puts
+ *  it into the Ready_Queue
+ *  - cpu() : simulates the operations of the CPUs
+ */
