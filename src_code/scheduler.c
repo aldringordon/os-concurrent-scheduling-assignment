605e485bfa59fc50aaee5853bfc818d7a7d8b68f
AldrinHome <aldrin.gordon@outlook.com>
2019-04-21 17:17:47 +0800

    main function template

--- src_code/scheduler.c
+++ src_code/scheduler.c
@@ -5,8 +5,43 @@
  * Purpose: Main Program that contains the tasks
  * in the assignment specification.
  *
+ * Should be called as:
+ *  $ ./scheduler task_file m
+ *
  * Specified required functions:
  *  - task() : gets two tasks at a time and puts
  *  it into the Ready_Queue
  *  - cpu() : simulates the operations of the CPUs
+ *
+ * References have been annotated at each line with an
+ * index to the specification at the end of the program.
+ */
+ #include <std.io>
+ #include <stdlib.h>
+
+
+
+int main(int argc, char* argv[])
+{
+    if(argc < 3)
+    {
+        printf("Error - Usage: ./scheduler task_file m\n");
+        return EXIT_FAILURE; /* See Reference #1 */
+    }
+    else
+    {
+
+    }
+    return EXIT_SUCCESS; /* See Reference #1 */
+}
+
+/**
+ * References:
+ *
+ * #1: https://stackoverflow.com/questions/9549342/should-i-return-0-or-1-for-successful-function
+ *      "For greater portability, you can use the macros EXIT_SUCCESS and
+ *      EXIT_FAILURE for the conventional status value for success and failure,
+ *      respectively. They are declared in the file stdlib.h.""
+ *
+ *
  */
