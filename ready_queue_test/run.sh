4d4fba75424b762bf30c290d35b4aa67e00eb323
AldrinHome <aldrin.gordon@outlook.com>
2019-04-22 22:48:42 +0800

    ready_queue.c TESTED AND WORKS

--- ready_queue_test/run.sh
+++ ready_queue_test/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+python task_file_generator.py
 gcc -g -c test_queue.c ready_queue.c
 gcc test_queue.o ready_queue.o -o test_queue
 ./test_queue
-rm -rf test_queue.o ready_queue.o test_queue
+rm -rf test_queue.o ready_queue.o test_queue task_file
