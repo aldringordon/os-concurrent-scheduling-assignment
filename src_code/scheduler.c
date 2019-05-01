53cd83e1040b7eb749f3bb0eebcc05aebd015a9a
Aldrin Gordon <aldrin.gordon@student.curtin.edu.au>
2019-05-01 13:19:54 +0800

    attempted task() and cpu() with pthread functions

--- src_code/scheduler.c
+++ src_code/scheduler.c
@@ -18,11 +18,17 @@
  */
 #include <std.io>
 #include <stdlib.h>
-#include <time.h> /* See Reference #2 */
+#include <time.h> /* Reference #2 */
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h> /* Reference #3 */
 
 int num_tasks, total_waiting_time, total_turnaround_time;
+ReadyQueue* ready_queue;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
 
 /**
  * places files from the Job-Queue into the Ready-Queue
@@ -30,10 +36,37 @@ int num_tasks, total_waiting_time, total_turnaround_time;
  */
 void* task(JobQueue** job_queue)
 {
+    Task* task;
     do
     {
+        if((*job_queue)->size > 1)
+        {
+            task = getJob(job_queue);
+            addTask(&ready_queue, task);
+        }
+        // OUTPUT TO SIMLOG
 
-    }while(*job_queue->size != 0);
+        pthread_cond_wait(&empty, &mutex);
+        pthread_mutex_lock(&mutex);
+
+        if((*job_queue)->size > 1)
+        {
+            task = getJob(job_queue);
+            addTask(&ready_queue, task);
+            // OUTPUT TO SIMLOG
+        }
+
+        task = getJob(job_queue);
+        addTask(&ready_queue, task);
+        // OUTPUT TO SIMLOG
+
+        pthread_mutex_unlock(&mutex);
+        pthread_cond_signal(&full, &mutex);
+
+
+    }while((*job_queue)->size != 0);
+
+    signalEmptyJobPool(&ready_queue);
 }
 
 /**
@@ -41,18 +74,30 @@ void* task(JobQueue** job_queue)
  * its "entire cpu burst" sleep(burst/10)
  * @param ready_queue [description]
  */
-void* cpu(ReadyQueue** ready_queue)
+void* cpu()
 {
+    Task *task;
     do
     {
+        pthread_cond_wait(&full, &mutex);
+        pthread_mutex_lock(&mutex);
 
-    }while(*ready_queue->size != 0);
+        task1 = getTask(&ready_queue);
+        // OUTPUT SERVICE_TIME TO SIMLOG
+
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&empty, &mutex);
+
+        sleep((task->)/25);
+        // OUTPUT COMPLETITION_TIME TO SIMLOG
+        free(task);
+
+    }while(*ready_queue->jobs_left != 0);
 }
 
 int main(int argc, char* argv[])
 {
     JobQueue* job_queue;
-    ReadyQueue* ready_queue;
 
     num_tasks = 0;
     total_waiting_time = 0;
@@ -72,13 +117,20 @@ int main(int argc, char* argv[])
         createReadyQueue(&ready_queue, argv[2]); /* argv[2] = m (capacity) */
 
         /* create 4 threads: 1 for task, 3 for cpus */
-        pthread_create(&task, ~ , ~ , ~ );
-        pthread_create(&cpu1, ~ , ~ , ~ );
-        pthread_create(&cpu2, ~ , ~ , ~ );
-        pthread_create(&cpu3, ~ , ~ , ~ );
+        pthread_create(&task, NULL , task, &job_queue);
+        pthread_create(&cpu1, NULL , cpu, NULL);
+        pthread_create(&cpu2, NULL , cpu, NULL);
+        pthread_create(&cpu3, NULL , cpu, NULL);
 
+        /* block until all thread complete Reference #4 */
+        pthread_join(task, NULL);
+        pthread_join(cpu1, NULL);
+        pthread_join(cpu2, NULL);
+        pthread_join(cpu3, NULL);
 
-
+        /* cleanup */
+        free(job_queue);
+        free(ready_queue);
     }
     return EXIT_SUCCESS; /* See Reference #1 */
 }
@@ -93,4 +145,10 @@ int main(int argc, char* argv[])
  *
  * #2: https://www.tutorialspoint.com/c_standard_library/time_h.htm
  *      For time and date related methods.
+ *
+ * #3: https://www.geeksforgeeks.org/use-posix-semaphores-c/
+ *      Using posix semaphores
+ *
+ * #4: https://randu.org/tutorials/threads/
+ *      Example program using pthread functions
  */
