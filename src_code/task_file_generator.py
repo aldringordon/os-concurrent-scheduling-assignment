a1a2001de0d0c4389cba1d2a87bf57ee6d11fc17
AldrinHome <aldrin.gordon@outlook.com>
2019-05-04 15:56:11 +0800

    asks user for task file size

--- src_code/task_file_generator.py
+++ src_code/task_file_generator.py
@@ -7,11 +7,13 @@
 # Note: this program has nothing to do with the
 # implementation of the simulation itself, i just
 # dont want to create a task_file of 100 tasks manually
+import sys
 import random
 
+tasks = int(input("Enter number of tasks: "))
 file = open("task_file","w")
 
-for i in range(1, 101):
+for i in range(1, tasks+1):
     file.write(str(i) + ' ' + str(random.randint(1,50)) + '\n')
 
 file.close()
